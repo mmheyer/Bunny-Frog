b924c38a5a47cc5980307ee8804b7b0c12eda812
yousol <yousol@umich.edu>
2022-05-17 21:59:50 +0900

    part 4 done

--- Steve.gd
+++ Steve.gd
@@ -8,8 +8,16 @@ const GRAVITY = 35
 func _physics_process(delta):
 	if Input.is_action_pressed("right"):
 		velocity.x = SPEED
-	if Input.is_action_pressed("left"):
+		$Sprite.play("walk")
+		$Sprite.flip_h = false
+	elif Input.is_action_pressed("left"):
 		velocity.x = -SPEED
+		$Sprite.play("walk")
+		$Sprite.flip_h = true
+	else:
+		$Sprite.play("idle")
+	if not is_on_floor():
+		$Sprite.play("air")
 	
 	velocity.y = velocity.y + GRAVITY
 	
