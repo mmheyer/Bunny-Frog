a3f06e7974ec2e986ce8905fee8675ed59bf3b9c
mmheyer <mmheyer@umich.edu>
2022-06-30 15:43:59 -0400

    fixed title screen. began working on level select menu.

---
+++ SettingsButton.gd
@@ -0,0 +1,4 @@
+extends MenuButton
+
+func _ready():
+	$SettingsButton.get_popup().add_item("Music")
---
+++ TitleMenu.gd
@@ -0,0 +1,4 @@
+extends Control
+
+func _ready():
+	$SettingsButton.get_popup().add_item("Music")
