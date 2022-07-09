b123a0ea4096c027b6beaa070c68c17a54cc4d95
yousol <yousol@umich.edu>
2022-07-08 23:04:57 -0700

    added splash frames to crocodile anim

--- Crocodile.gd
+++ Crocodile.gd
@@ -9,6 +9,7 @@ func _process(_delta):
 	var collision3 := get_node(str("Coll3")) as CollisionPolygon2D
 	var collision4 := get_node(str("Coll4")) as CollisionPolygon2D
 	var collision5 := get_node(str("Coll5")) as CollisionPolygon2D
+	var collision6 := get_node(str("Coll6")) as CollisionPolygon2D
 	if sprite.frame == 0:
 		collision5.disabled = true
 		collision1.disabled = true
@@ -19,19 +20,21 @@ func _process(_delta):
 	elif sprite.frame == 1:
 		collision0.disabled = true
 		collision1.disabled = false
-	elif sprite.frame == 2 || sprite.frame == 3 || sprite.frame == 5 || sprite.frame == 6:
+	elif sprite.frame == 2 || sprite.frame == 3:
 		collision1.disabled = true
-		collision3.disabled = true
 		collision2.disabled = false
 	elif sprite.frame == 4:
 		collision2.disabled = true
 		collision3.disabled = false
-	elif sprite.frame == 7 || sprite.frame == 8 || sprite.frame == 9:
+	elif sprite.frame == 5:
 		collision3.disabled = true
 		collision4.disabled = false
-	elif sprite.frame == 10:
+	elif sprite.frame == 6 || sprite.frame == 7 || sprite.frame == 8:
 		collision4.disabled = true
 		collision5.disabled = false
+	elif sprite.frame == 9:
+		collision5.disabled = true
+		collision6.disabled = false
 	else:
 		collision0.disabled = true
 		collision1.disabled = true
@@ -39,6 +42,7 @@ func _process(_delta):
 		collision3.disabled = true
 		collision4.disabled = true
 		collision5.disabled = true
+		collision6.disabled = true
 
 func _on_Area2D_body_entered(body):
 	if body.name == "bunnyfrog":
