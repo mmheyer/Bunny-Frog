4010c69732ed2e81af308df3f43fe9c411342f41
mmheyer <mmheyer@umich.edu>
2022-07-22 12:52:11 -0400

    coded flag pole!

---
+++ FlagPolev2.gd
@@ -0,0 +1,24 @@
+extends Area2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+onready var flag_pole = get_node("AnimatedSprite")
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_FlagPolev2_body_entered(body):
+	flag_pole.play("default")
+	yield( flag_pole, "animation_finished" )
+	get_tree().change_scene("res://scenes/levels/LevelSelect.tscn")
