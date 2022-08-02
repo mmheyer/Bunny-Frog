6670dac4eb9da28594ad5fffd1f21dbb2f1ad238
Amy Ham <amy@Amys-MBP.lan>
2022-08-02 14:41:59 -0700

    fixed bee

--- Bee.gd
+++ Bee.gd
@@ -1,4 +1,5 @@
 extends Area2D
+export var speed = 300
 
 func _on_Area2D_body_entered(body):
 	if body.name == "bunnyfrog":
--- BeePathFollow2D.gd
+++ BeePathFollow2D.gd
@@ -1,6 +1,11 @@
 extends PathFollow2D
 
-export var flyspeed = 300
+export var speed = 300
 
 func _process(delta):
-	set_offset(get_offset() + flyspeed * delta)
+	set_offset(get_offset() + speed * delta)
+
+
+func _on_Area2D_body_entered(body):
+	if body.name == "bunnyfrog":
+		speed = 0
