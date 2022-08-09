b128fda0d6e1e44194a89f671160f4e415b6c96e
Amy Ham <amy@Amys-MBP.lan>
2022-08-09 00:03:35 -0700

    rotating spike platform fixes

--- RotatingSpikePlatform.gd
+++ RotatingSpikePlatform.gd
@@ -1,4 +1,4 @@
-extends Area2D
+extends Node2D
 
 export var rotation_speed = 1.1
 
@@ -6,12 +6,18 @@ func _ready():
 	$AnimationPlayer.play("rotate")
 	
 func _process(delta):
-	$KinematicBody2D/Sprite/pivot.rotation += rotation_speed * delta
-	$KinematicBody2D/Sprite/pivot/Block1.global_rotation = 0
-	$KinematicBody2D/Sprite/pivot/Block2.global_rotation = 0
-	$KinematicBody2D/Sprite/pivot/Block3.global_rotation = 0
-	$KinematicBody2D/Sprite/pivot/Block4.global_rotation = 0
+	$KinematicBody2D.rotation += rotation_speed * delta
+	$KinematicBody2D/pivot/Block1.global_rotation = 0
+	$KinematicBody2D/Collision1.global_rotation = 0
+	$KinematicBody2D/pivot/Block2.global_rotation = 0
+	$KinematicBody2D/Collision2.global_rotation = 0
+	$KinematicBody2D/pivot/Block3.global_rotation = 0
+	$KinematicBody2D/Collision3.global_rotation = 0
+	$KinematicBody2D/pivot/Block4.global_rotation = 0
+	$KinematicBody2D/Collision4.global_rotation = 0
 
-func _on_Area2D_body_entered(body):
+func _on_Group_body_entered(body):
 	if body.name == "bunnyfrog":
 		body.die()
+		$AnimationPlayer.stop()
+		rotation_speed = 0
--- RotatingSpikes.gd
+++ RotatingSpikes.gd
@@ -6,3 +6,4 @@ func _ready():
 func _on_Area2D_body_entered(body):
 	if body.name == "bunnyfrog":
 		body.die()
+		$AnimationPlayer.stop()
